0f546a5255e596f2f03e43cd2a90402c9d8171f8
TigerSiunk <bmy_13@hotmail.fr>
2022-05-09 13:26:16 +0200

    flake8 ok

--- src/app.py
+++ src/app.py
@@ -2,11 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def index() :
+def index():
     return "Hello World !"
+
+
 @app.route("/attaque")
 def attaque():
     return "Attaque effectuée"
+
+
 if __name__ == "__main__":
     app.run()
