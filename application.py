55b42fc40e196a0faf1bb1f87f0340c93e9e899c
TigerSiunk <bmy_13@hotmail.fr>
2022-05-10 07:37:07 +0200

    app.py replaced to application.py because elastic beanstalk check for an application.py file to run server

---
+++ application.py
@@ -0,0 +1,23 @@
+from flask import Flask
+
+application = Flask(__name__)
+
+
+@application.route("/")
+def index():
+    return "Hello World !"
+
+
+@application.route("/attaque")
+def attaque():
+    return "Attaque effectuée"
+
+
+def fonction_retourne_int(integer):
+    if type(integer) == int:
+        return 10
+    return "non"
+
+
+if __name__ == "__main__":
+    application.run()
